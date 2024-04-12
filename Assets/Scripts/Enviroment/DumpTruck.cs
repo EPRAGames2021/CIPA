71cd5de7d3c293b485fb42e9b592c70274e4eddd
paulosaraty <paulovsaraty@gmail.com>
2024-04-12 16:23:55 -0300

    Dump truck now animated correctly

---
+++ Assets/Scripts/Enviroment/DumpTruck.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DumpTruck : MonoBehaviour
+{
+    [SerializeField] private Animator _animator;
+
+    [SerializeField] private ScreenTouchController _controller;
+
+    private void OnValidate()
+    {
+        if (_animator == null)
+        {
+            _animator = GetComponent<Animator>();
+        }
+    }
+
+    private void Update()
+    {
+        if (_controller == null)
+        {
+            Debug.Log("Controller is null");
+
+            return;
+        }
+
+        Dump(_controller.DetectHolding());
+    }
+
+    public void Dump(bool dump)
+    {
+        _animator.SetBool("IsDumping", dump);
+    }
+}
