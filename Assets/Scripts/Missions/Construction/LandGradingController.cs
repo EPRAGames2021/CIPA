1ee76affe78e79079d384460cb349e1735443097
paulosaraty <paulovsaraty@gmail.com>
2024-06-13 14:12:15 -0300

    Bug fixes
    Fixed a bug where day 2 of construction could be beaten simply by confirming wrong ingredients

--- Assets/Scripts/Missions/Construction/LandGradingController.cs
+++ Assets/Scripts/Missions/Construction/LandGradingController.cs
@@ -5,26 +5,39 @@ namespace CIPA
 {
     public class LandGradingController : MonoBehaviour
     {
+        [SerializeField] private GameObject _minigameUI;
+
         private void OnEnable()
         {
+            _minigameUI.SetActive(false);
+
             CanvasManager.Instance.EnableVirtualJoystick(true);
             CanvasManager.Instance.EnableHUD(true);
 
-            CustomGameEvents.OnMinigameStarted += StartMiniGame;
+            CustomGameEvents.OnPlayerArrivedAtMinigameLocation += StartMiniGame;
+            CustomGameEvents.OnMinigameStarted += ActivateUI;
         }
 
         private void OnDisable()
         {
-            CustomGameEvents.OnMinigameStarted -= StartMiniGame;
+            CustomGameEvents.OnPlayerArrivedAtMinigameLocation -= StartMiniGame;
+            CustomGameEvents.OnMinigameStarted -= ActivateUI;
         }
 
 
         private void StartMiniGame()
         {
+            _minigameUI.SetActive(true);
+
             CanvasManager.Instance.EnableVirtualJoystick(false);
             CanvasManager.Instance.EnableHUD(false);
 
             JobAreaManager.Instance.ArrivedAtMinigameLocation = true;
+        }
+
+        private void ActivateUI()
+        {
+            _minigameUI.SetActive(true);
         }
     }
 }
