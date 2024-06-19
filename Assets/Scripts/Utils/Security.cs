99c9329378cda5307d71983e1cbcc0512d9e66ef
Leo Baiano <ljunior2005@gmail.com>
2024-06-19 17:58:48 -0300

    feat: adiciona classe Security para lidar com questões relacionadas a segurança como SQL Injection e Criptografia

---
+++ Assets/Scripts/Utils/Security.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Data.SqlClient;
+using System.Security.Cryptography;
+using System.Text;
+
+public class DatabaseHelper
+{
+    private readonly string _connectionString;
+
+    public DatabaseHelper(string connectionString)
+    {
+        _connectionString = connectionString;
+    }
+
+    // Método para executar uma consulta SQL parametrizada
+    public void ExecuteQuery(string query, SqlParameter[] parameters)
+    {
+        using (SqlConnection connection = new SqlConnection(_connectionString))
+        {
+            SqlCommand command = new SqlCommand(query, connection);
+            command.Parameters.AddRange(parameters);
+            connection.Open();
+            command.ExecuteNonQuery();
+        }
+    }
+
+    // Método para obter dados de uma consulta SQL parametrizada
+    public SqlDataReader ExecuteQueryWithResult(string query, SqlParameter[] parameters)
+    {
+        SqlConnection connection = new SqlConnection(_connectionString);
+        SqlCommand command = new SqlCommand(query, connection);
+        command.Parameters.AddRange(parameters);
+        connection.Open();
+        return command.ExecuteReader(CommandBehavior.CloseConnection);
+    }
+
+    // Método para criptografar uma senha usando SHA-256
+    public string EncryptPassword(string password)
+    {
+        using (SHA256 sha256Hash = SHA256.Create())
+        {
+            byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(password));
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                builder.Append(bytes[i].ToString("x2"));
+            }
+            return builder.ToString();
+        }
+    }
+}
